1e0f267149bfedd03cade9e354ac29b0e6c5256b
Joe Avallone <allsystemsgo_inc@yahoo.com>
2022-09-16 12:43:20 -0400

    check in server list guard

--- sgm/src/app/app-routing.module.ts
+++ sgm/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoute } from './enums/app-config-interal';
+import { ServerListGuard } from './guards/server-list.guard';
 
 import { LoginComponent } from './screens/login/login.component';
 import { ServerListComponent } from './screens/server-list/server-list.component';
@@ -9,7 +10,7 @@ import { ServerListComponent } from './screens/server-list/server-list.component
 
 const routes: Routes = [
   { path: AppRoute.Login, component: LoginComponent, pathMatch: 'full'},
-  { path: AppRoute.ServerList, component: ServerListComponent, pathMatch: 'full'},
+  { path: AppRoute.ServerList, component: ServerListComponent, pathMatch: 'full', canActivate:[ServerListGuard]},
   { path: AppRoute.Default, component: LoginComponent, pathMatch: 'full'},
 ];
 
